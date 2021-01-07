2c7592e440789710ad4877eb765b7f2809e29b83
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-06 19:23:34 -0800

    Sapper Netlify template

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,9 @@
+<!-- Layout is for any layout components like Header/Nav, Footer, or a container. 
+  Similar to layout in Gatsby, present on all pages -->
+<script>
+</script>
+
+<style>
+</style>
+
+<slot />
