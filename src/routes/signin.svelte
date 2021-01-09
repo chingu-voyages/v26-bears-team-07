04d06359793ec9aa91a3d327e2e1b7276bb29b8a
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-09 14:25:05 -0800

    password field

--- src/routes/signin.svelte
+++ src/routes/signin.svelte
@@ -5,8 +5,14 @@
     min-height: 100vh;
     padding: 48px 40px 36px;
   }
+  h1 {
+    text-align: center;
+  }
   input {
     width: 100%;
+  }
+  input[type="password"] {
+    margin-top: 1rem;
   }
   button {
     display: block;
@@ -20,7 +26,9 @@
 
 <main>
   <section>
+    <h1>Sign In</h1>
     <input type="text" placeholder="Email or username" />
+    <input type="password" placeholder="Enter your password" />
     <button>Sign In</button>
   </section>
 </main>
