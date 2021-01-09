dec0ed83e72bd2e88867116a68e1265096c4c0dd
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-08 17:32:22 -0800

    basic signin form

---
+++ src/routes/signin.svelte
@@ -0,0 +1,26 @@
+<style>
+  main {
+    display: grid;
+    place-items: center;
+    min-height: 100vh;
+    padding: 48px 40px 36px;
+  }
+  input {
+    width: 100%;
+  }
+  button {
+    display: block;
+    margin: 2rem 0 0 auto;
+  }
+  section {
+    width: 100%;
+  }
+  /* TODO: Mobile dimensions at min-width 601px */
+</style>
+
+<main>
+  <section>
+    <input type="text" placeholder="Email or username" />
+    <button>Sign In</button>
+  </section>
+</main>
