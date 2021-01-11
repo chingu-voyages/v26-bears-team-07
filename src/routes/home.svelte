40be78ca452187f2cf8882042e077f37fb83bb52
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-11 02:42:28 -0800

    basic class creation form

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,1 +1,18 @@
+<script>
+  let createOpen = false;
+</script>
+
 <h1>Classroom</h1>
+<button on:click={() => (createOpen = true)}>Create class</button>
+
+{#if createOpen}
+  <form on:submit|preventDefault>
+    <h2>Create class</h2>
+    <input type="text" placeholder="Class name (required)" required />
+    <input type="text" placeholder="Section" />
+    <input type="text" placeholder="Subject" />
+    <input type="text" placeholder="Room" />
+
+    <button type="submit">Create</button>
+  </form>
+{/if}
