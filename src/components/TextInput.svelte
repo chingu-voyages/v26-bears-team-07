c6d4e54c3602fde5e8a355fabbdf2d22c8473e43
iamtimleonard <timleonardcello@gmail.com>
2021-01-16 18:31:57 -0500

    create Button and TextInput

---
+++ src/components/TextInput.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let controlType = null;
+  export let placeholder;
+  export let rows = null;
+  export let type = "text";
+  export let value;
+
+  let touched = false;
+</script>
+
+<div class="form-control">
+  {#if controlType === "textarea"}
+    <textarea {rows} bind:value />
+  {:else}
+    <input {type} {placeholder} />
+  {/if}
+</div>
+
+<style>
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    border: none;
+    border-bottom: 2px solid #eee;
+    border-radius: 3px 3px 0 0;
+    background: rgb(250, 250, 250);
+    padding: 0.5rem 0.25rem;
+    transition: border-color 0.1s ease-out;
+  }
+
+  input:focus,
+  textarea:focus {
+    border-color: #1967d2;
+    outline: none;
+  }
+
+  .form-control {
+    padding: 0.5rem 0;
+    width: 100%;
+    margin: 0.25rem 0;
+  }
+</style>
