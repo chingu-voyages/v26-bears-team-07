1e87b16ea307145a5aa4e839cb9a17eee11c2287
iamtimleonard <timleonardcello@gmail.com>
2021-01-16 18:42:52 -0500

    fix spacing for mobile

--- src/components/TabItem.svelte
+++ src/components/TabItem.svelte
@@ -16,7 +16,7 @@
   }
 
   .active:hover {
-    background-color: rgba(25, 103, 210, 0.04);
+    background-color: rgba(25, 103, 210, 0.06);
   }
 
   .tab-item {
@@ -34,8 +34,8 @@
     background-color: rgba(238, 238, 238, 0.514);
   }
   @media (max-width: 800px) {
-    .tab-item a {
-      padding: 30px 10px;
+    a {
+      padding: px 10px;
     }
   }
 </style>
