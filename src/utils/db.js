d05ccb7f086f6f97936b641245deb26a4ad26e56
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-16 22:39:26 -0800

    enable preview partial update + log in dev mode

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,12 +7,16 @@ export async function query(secret, { query, variables = {} }) {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${secret}`,
+      "X-Schema-Preview": "partial-update-mutation",
     },
     body: JSON.stringify({ query, variables }),
   }).then((response) => response.json());
 
   // You can handle errors here if needed
   //    They'll be on result.errors
+  if (process.env.NODE_ENV == "development") {
+    console.log(result);
+  }
 
   return result.data;
 }
