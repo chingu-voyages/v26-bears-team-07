bc073a21c7cde9802d99eaa769d7d35c0ad2e6dd
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-16 20:56:59 -0800

    working example class creation query

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,17 +1,34 @@
 <script>
+  import { authStore } from "../stores/auth";
+  import { createClass } from "../utils/class";
   let createOpen = false;
+  let name, section, subject, room;
 </script>
 
 <h1>Classroom</h1>
 <button on:click={() => (createOpen = true)}>Create class</button>
 
 {#if createOpen}
-  <form on:submit|preventDefault>
+  <form
+    on:submit|preventDefault={async () =>
+      await createClass($authStore.secret, {
+        name,
+        id: $authStore.id,
+        section,
+        subject,
+        room,
+      })}
+  >
     <h2>Create class</h2>
-    <input type="text" placeholder="Class name (required)" required />
-    <input type="text" placeholder="Section" />
-    <input type="text" placeholder="Subject" />
-    <input type="text" placeholder="Room" />
+    <input
+      type="text"
+      bind:value={name}
+      placeholder="Class name (required)"
+      required
+    />
+    <input type="text" bind:value={section} placeholder="Section" />
+    <input type="text" bind:value={subject} placeholder="Subject" />
+    <input type="text" bind:value={room} placeholder="Room" />
 
     <button type="submit">Create</button>
   </form>
