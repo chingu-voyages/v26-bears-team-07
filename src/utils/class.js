8bf396cd350cc10f71d1499d16c7f027be8a02f6
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-16 19:07:17 -0800

    add the class creation util

---
+++ src/utils/class.js
@@ -0,0 +1,28 @@
+import { query, optProps } from "./db";
+
+export const createClass = (
+  secret,
+  { name, id, link, invite, section, subject, room }
+) =>
+  query(secret, {
+    query: `
+mutation CreateClass {
+  createClass(
+    data: {
+      name: "${name}"
+      owner: { connect: ${id} }
+      link: "${link}"
+      invite: "${invite}"
+      teachers: { connect: ${id} }
+      ${optProps({
+        section,
+        subject,
+        room,
+      })}
+    }
+  ) {
+    _id
+  }
+}
+`,
+  });
