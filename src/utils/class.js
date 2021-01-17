f46aff9126c9aaf1e0eaa26dda628b33afe30013
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-16 19:40:39 -0800

    add random link and invite generation and comments

--- src/utils/class.js
+++ src/utils/class.js
@@ -1,8 +1,27 @@
 import { query, optProps } from "./db";
 
 export const createClass = (
+  /**
+   * Creates a classroom. Must provide a name and owner id.
+   *
+   * Link and invite code can be auto-generated.
+   *
+   * Warning: Classes are unique by link. While not likely a random string is the same,
+   * it should be accounted for in a production environment, and errors should be dealt with.
+   */
   secret,
-  { name, id, link, invite, section = "", subject = "", room = "" }
+  {
+    name,
+    id,
+    // random link string if link not provided
+    link = Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15),
+    // random invite string of length 4
+    invite = Math.random().toString(36).substring(2, 6),
+    section = "",
+    subject = "",
+    room = "",
+  }
 ) =>
   query(secret, {
     query: `
@@ -22,6 +41,8 @@ mutation CreateClass {
     }
   ) {
     _id
+    link
+    invite
   }
 }
 `,
