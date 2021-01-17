bcfcb2840725275bc1fc3ffc67f1bc5b7660e4a2
jidemobell <jidd32@gmail.com>
2021-01-17 21:43:54 +0100

    implement table menu(basic function)

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Nav from "./Nav.svelte"
+    import SideNav from "./SideNav.svelte"
+    let open;
+
+    function openMenu(){
+      console.log('time to open menu')
+      open = true
+    }
+
+    function closeMenu(){
+      console.log('time to close menu')
+      open = false
+    }
+  </script>
+  
+  
+  <Nav on:opensidenav={() => openMenu()}/>
+  <SideNav openMenu={open} on:sidenavclosed={() => closeMenu()} />
