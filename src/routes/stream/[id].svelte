162a2f20e932fc9d76b2da8ec27cfa37796338f1
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-19 05:08:59 -0800

    test: netlify redirects for sapper dynamic routing

---
+++ src/routes/stream/[id].svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { id } = page.params;
+    return { id };
+  }
+</script>
+
+<script>
+  export let id;
+</script>
+
+<h1>Hey! {id}</h1>
