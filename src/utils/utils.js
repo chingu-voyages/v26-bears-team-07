5fe7b3e8b5830c6b6731cdb69124b5b4df28510e
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-18 19:56:28 -0800

    add random ID generator func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,28 +1,39 @@
 /** Dispatch event on click outside of node */
 export function clickOutside(node) {
-    const handleClick = event => {
-      if (node && !node.contains(event.target) && !event.defaultPrevented) {
-        node.dispatchEvent(
-          new CustomEvent('click_outside', node)
-        )
-      }
+  const handleClick = (event) => {
+    if (node && !node.contains(event.target) && !event.defaultPrevented) {
+      node.dispatchEvent(new CustomEvent("click_outside", node));
     }
-  
-      document.addEventListener('click', handleClick, true);
-    
-    return {
-      destroy() {
-        document.removeEventListener('click', handleClick, true);
-      }
-      }
-  }
+  };
+
+  document.addEventListener("click", handleClick, true);
+
+  return {
+    destroy() {
+      document.removeEventListener("click", handleClick, true);
+    },
+  };
+}
 
 /**capitalize first letter in a word */
-export function firstLetterToCap(letter){
-  return letter.replace(letter[0], letter[0].toUpperCase())
+export function firstLetterToCap(letter) {
+  return letter.replace(letter[0], letter[0].toUpperCase());
 }
 
 /**check if val is odd */
-export function isOdd(val){
-  return val & 1; 
+export function isOdd(val) {
+  return val & 1;
+}
+
+/** Returns a randomized human unreadable string.
+ * TODO: Currently unused. Remove if still unused on finished product
+ */
+export function randID() {
+  // You can customize and reduce the length of the random string by changing the second arg of substring
+  // or removing the duplicate call. See createClass func's invite field.
+  var random =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+
+  return random;
 }
