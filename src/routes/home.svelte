db92e49157a8a2ef0585a3d8e2f4d4d4a7ecb865
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-19 05:06:02 -0800

    basic classroom fetching logic

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,8 +1,21 @@
 <script>
+  import { onMount, tick } from "svelte";
   import { authStore } from "../stores/auth";
-  import { createClass } from "../utils/class";
+  import { classesByUser, createClass } from "../utils/class";
   let createOpen = false;
   let name, section, subject, room;
+
+  let classes = [];
+  onMount(async () => {
+    await tick;
+    if ($authStore.secret) {
+      var res = await classesByUser($authStore.secret, $authStore.id);
+      var {
+        findUserByID: { teaches, attends },
+      } = res;
+      classes = [...teaches.data, ...attends.data];
+    }
+  });
 </script>
 
 <h1>Classroom</h1>
@@ -33,3 +46,9 @@
     <button type="submit">Create</button>
   </form>
 {/if}
+
+{#each classes as { name, _id, invite }}
+  <a href="#">
+    <h2>{name}</h2>
+  </a>
+{/each}
