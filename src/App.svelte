cb2ef29320fe101c9b481d86a0ff6efca018175f
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-22 23:58:31 -0800

    initclient import from obj now

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
-  import initClient from "./utils/client";
+  import { initClient } from "./utils/client";
   import { authStore } from "./stores/auth";
 
   $authStore?.secret && initClient($authStore.secret);
