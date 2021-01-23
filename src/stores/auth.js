020f7f5cf1f00cdc60d0eb88fa23aac088c52233
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-22 20:08:14 -0800

    use `includes` for string logic instead

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,13 +4,13 @@ import { get, writable } from "svelte/store";
 /** Redirect if at a path that requires login and user is not logged in. */
 function checkPaths(arr) {
   var curPath = location.pathname;
-  if (!arr.filter((v) => v == curPath).length) location.pathname = `/signin`;
+  if (!arr.filter((v) => curPath.includes(v) || curPath == "/").length)
+    location.pathname = `/signin`;
 }
 
 /** Initializes with auth details. Redirect check is only performed once for the app's init. */
 export const authStore = writable(
-  JSON.parse(localStorage.getItem("auth")) ||
-    checkPaths(["/signin", "/signup", "/"])
+  JSON.parse(localStorage.getItem("auth")) || checkPaths(["signin", "signup"])
 );
 
 /** Initializes secret into store if logged in.
