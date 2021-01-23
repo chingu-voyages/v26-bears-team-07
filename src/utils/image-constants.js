e77b7f1670d16874e55796c057254f2221154def
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-22 16:56:47 -0800

    normalize the paths so the images load correctly

--- src/utils/image-constants.js
+++ src/utils/image-constants.js
@@ -1,11 +1,10 @@
 /** icons */
- export const hamburger = 'icons/menu.svg';
- export const plus = 'icons/plus.svg';
- export const archive = 'icons/archive.svg';
- export const calendar = 'icons/calendar.svg';
- export const settings = 'icons/settings.svg';
- export const review = 'icons/book-open.svg';
- export const classes = 'icons/book-open.svg';
- export const invite = 'icons/invite.svg';
- export const teacher = 'icons/announcer.png';
-
+export const hamburger = "/icons/menu.svg";
+export const plus = "/icons/plus.svg";
+export const archive = "/icons/archive.svg";
+export const calendar = "/icons/calendar.svg";
+export const settings = "/icons/settings.svg";
+export const review = "/icons/book-open.svg";
+export const classes = "/icons/book-open.svg";
+export const invite = "/icons/invite.svg";
+export const teacher = "/icons/announcer.png";
