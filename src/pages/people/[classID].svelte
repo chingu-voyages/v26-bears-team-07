db0a68f1a10438e62afb0cae242593d4bec5d099
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-23 04:29:04 -0800

    add data flow for people page

---
+++ src/pages/people/[classID].svelte
@@ -0,0 +1,27 @@
+<script>
+  import { params } from "@roxi/routify";
+  import { usersByClassID } from "../../stores/query";
+
+  let classData = usersByClassID({ classID: $params.classID });
+
+  let teachers = [];
+  let students = [];
+  $: if ($classData?.data) {
+    teachers = $classData.data.result.teachers.data;
+    students = $classData.data.result.students.data;
+  }
+</script>
+
+<div>
+  <h2>Teachers</h2>
+  {#each teachers as { name }}
+    <p>{name}</p>
+  {/each}
+</div>
+
+<div>
+  <h2>Students</h2>
+  {#each students as { name }}
+    <p>{name}</p>
+  {/each}
+</div>
