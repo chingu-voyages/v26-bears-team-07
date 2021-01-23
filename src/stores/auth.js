eabc812cfab7c2e58c0ad6bdd1bd0608c773794c
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-22 20:16:08 -0800

    remove unused imports

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,5 +1,4 @@
-import { onMount, tick } from "svelte";
-import { get, writable } from "svelte/store";
+import { writable } from "svelte/store";
 
 /** Redirect if at a path that requires login and user is not logged in. */
 function checkPaths(arr) {
