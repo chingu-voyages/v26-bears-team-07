6190d2e7062b94545b2f5884b42286595b92755c
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-01-22 19:51:31 -0800

    render header only if logged in

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -2,10 +2,14 @@
   Similar to layout in Gatsby, present on all pages -->
 <script>
   import Header from "../components/Header/Header.svelte";
+  import { authStore } from "../stores/auth";
 </script>
 
 <!-- routify:options preload="proximity" -->
-<Header />
+<!-- render header only if logged in -->
+{#if $authStore?.secret}
+  <Header />
+{/if}
 <slot />
 
 <style>
