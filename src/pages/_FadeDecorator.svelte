8f09ab7a47d216ffcb19aa10b600417c3ce3d071
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-23 16:01:30 -0800

    improve page transitions

--- src/pages/_FadeDecorator.svelte
+++ src/pages/_FadeDecorator.svelte
@@ -1,18 +1,19 @@
 <!-- _FadeDecorator.svelte -->
 <script>
-  import { BaseTransition } from "@roxi/routify/decorators";
+  import { beforeUrlChange } from "@roxi/routify";
   import { fade } from "svelte/transition";
+  import { sleep } from "../utils/utils";
 
-  const configs = [
-    {
-      condition: (meta) => true,
-      transition: fade,
-      inParams: { duration: 200 },
-      outParams: { duration: 0 },
-    },
-  ];
+  let isReady = true;
+  $beforeUrlChange(async () => {
+    isReady = false;
+    await sleep(150);
+    return true;
+  });
 </script>
 
-<BaseTransition {configs}>
-  <slot />
-</BaseTransition>
+{#if isReady}
+  <div out:fade={{ duration: 150 }} in:fade={{ duration: 200 }}>
+    <slot />
+  </div>
+{/if}
