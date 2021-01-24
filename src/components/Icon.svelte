85848331ad788720a15724a1269ace7cd0881f04
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-23 18:28:08 -0800

    format

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,27 +1,27 @@
 <script>
-   export let name;
-   export let styles;
-   
-   function getAlt(name){
-      return name.split('/')[1]
-   }
+  export let name;
+  export let styles;
+
+  function getAlt(name) {
+    return name.split("/")[1];
+  }
 </script>
 
+<img alt={getAlt(name)} src={name} on:click style={styles} />
+
 <style>
-   img {
-    margin: 0 10px; 
+  img {
+    margin: 0 10px;
     width: 30px;
     font-weight: bolder;
     cursor: pointer;
     padding: 5px;
     margin-right: 20px;
     color: r#3c4043;
-   }
+  }
 
-   img:hover{
-      background-color: rgb(245, 245, 245);
-      border-radius: 50%;
-   }
+  img:hover {
+    background-color: rgb(245, 245, 245);
+    border-radius: 50%;
+  }
 </style>
-
-<img alt={getAlt(name)} src={name} on:click style={styles} />
