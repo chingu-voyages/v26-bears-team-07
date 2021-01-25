d977dc38880afd8d18a1b5699f9611f6bea4aefa
iamtimleonard <timleonardcello@gmail.com>
2021-01-24 22:07:49 -0500

    create basic classwork route and new assignment form

--- src/pages/classwork/[classID].svelte
+++ src/pages/classwork/[classID].svelte
@@ -1,9 +1,15 @@
 <script>
   import Button from "../../components/Header/Button.svelte";
   import { params } from "@roxi/routify";
+  import NewAssignment from "../../components/NewAssignment.svelte";
 
   console.log($params.classID);
+  let showForm = false;
 </script>
+
+{#if showForm}
+  <NewAssignment on:click={() => (showForm = false)} />
+{/if}
 
 <div class="container">
   <aside class="topics-menu">
@@ -16,12 +22,34 @@
   </aside>
   <main>
     <!-- dropdown menu for mobile -->
-    <Button type="confirm-filled">Create</Button>
+    <select>
+      <option value="">All topics</option>
+      <option value="topic 1">topic 1</option>
+      <option value="topic 2">topic 2</option>
+      <option value="topic 3">topic 3</option>
+      <option value="topic 4">topic 4</option>
+      <option value="topic 5">topic 5</option>
+    </select>
+    <Button on:click={() => (showForm = true)} type="confirm-filled"
+      >Create</Button
+    >
     <div class="content">
-      <div class="assignment">Assignment 1</div>
-      <div class="assignment">Assignment 2</div>
-      <div class="assignment">Assignment 3</div>
-      <div class="assignment">Assignment 4</div>
+      <div class="assignment">
+        <div class="icon" />
+        Assignment 1
+      </div>
+      <div class="assignment">
+        <div class="icon" />
+        Assignment 2
+      </div>
+      <div class="assignment">
+        <div class="icon" />
+        Assignment 3
+      </div>
+      <div class="assignment">
+        <div class="icon" />
+        Assignment 4
+      </div>
     </div>
   </main>
 </div>
@@ -35,6 +63,15 @@
 
   aside {
     width: 240px;
+  }
+
+  select {
+    display: none;
+    width: 100%;
+    padding: 0.75rem;
+    margin: 0 0 1rem 0;
+    border: 1px solid #dedede;
+    border-radius: 5px;
   }
 
   ul {
@@ -51,13 +88,31 @@
   }
 
   .assignment {
-    height: 2rem;
-    margin: 0.5rem 0;
+    height: 3.75rem;
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .icon {
+    width: 36px;
+    height: 36px;
+    background-color: #ccc;
+    margin: 0 1rem;
+    border-radius: 50%;
+  }
+
+  .assignment:hover {
+    box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.212);
   }
 
   @media (max-width: 960px) {
     aside {
       display: none;
+    }
+    select {
+      display: inherit;
     }
   }
 </style>
