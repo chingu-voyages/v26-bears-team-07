ee7f3f805519fa476a4b35e355b39cf7a8ebe0f3
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-28 01:14:09 -0800

    style invite page

--- src/pages/invite/[classID].svelte
+++ src/pages/invite/[classID].svelte
@@ -31,11 +31,59 @@
 </script>
 
 {#if isValid}
-  <button
-    on:click={async () => {
-      joinClass({ userID: $authStore.id, classID });
-    }}>Join Class</button
-  >
+  <div class="box center">
+    <div class="top center">
+      <h2>Bear Class</h2>
+      <p>Class helps classes communicate, save time, and stay organized.</p>
+    </div>
+    <div class="bottom center">
+      <p>You are joining this class as a student.</p>
+      <button
+        on:click={async () => {
+          joinClass({ userID: $authStore.id, classID });
+        }}>Join Class</button
+      >
+    </div>
+  </div>
 {:else if !$classData.fetching}
-  <h2>Invalid invite or not logged in.</h2>
+  <h2>Class not found</h2>
+  <p>Look for it on classes or double-check your link</p>
 {/if}
+
+<style>
+  .center {
+    display: grid;
+    place-items: center;
+  }
+  .box {
+    margin: 0 auto;
+    width: 47.5rem;
+    max-width: 93%;
+    border: 0.0625rem solid #dadce0;
+    border-radius: 0.5rem;
+    margin-top: 2rem;
+    overflow: hidden;
+  }
+  p,
+  h2 {
+    text-align: center;
+    width: 100%;
+    max-width: 24rem;
+  }
+  .top {
+    background: #f1f3f4;
+    padding: 2rem 0;
+    width: 100%;
+    min-height: 9rem;
+  }
+  .top p {
+    font-size: 0.9rem;
+  }
+  .bottom {
+    padding: 2rem 0;
+    min-height: 10rem;
+  }
+  .bottom p {
+    font-size: 0.8rem;
+  }
+</style>
