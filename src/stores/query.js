ba20c5d9a03b0bd077bf7a9fc2335e7e70e26922
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-01-30 18:10:41 -0800

    use gql tag

--- src/stores/query.js
+++ src/stores/query.js
@@ -1,11 +1,12 @@
 import { queryOp, useMutation } from "../utils/client";
+import { gql } from "@urql/svelte";
 
-const CLASS_FIELDS = `
-fragment ClassFields on Class {
-  name
-  invite
-  _id
-}
+const CLASS_FIELDS = gql`
+  fragment ClassFields on Class {
+    name
+    invite
+    _id
+  }
 `;
 
 /** Creates a classroom. Must provide a name and owner id.
@@ -15,31 +16,31 @@ fragment ClassFields on Class {
  */
 export const useCreateClass = () =>
   useMutation(
-    `
-mutation CreateClass(
-  $name: String!
-  $id: ID!
-  $invite: ID!
-  $section: String
-  $subject: String
-  $room: String
-) {
-  result: createClass(
-    data: {
-      name: $name
-      owner: { connect: $id }
-      invite: $invite
-      teachers: { connect: [$id] }
-      section: $section
-      subject: $subject
-      room: $room
-    }
-  ) {
-    ...ClassFields
-  }
-}
-${CLASS_FIELDS}
-`,
+    gql`
+      mutation CreateClass(
+        $name: String!
+        $id: ID!
+        $invite: ID!
+        $section: String
+        $subject: String
+        $room: String
+      ) {
+        result: createClass(
+          data: {
+            name: $name
+            owner: { connect: $id }
+            invite: $invite
+            teachers: { connect: [$id] }
+            section: $section
+            subject: $subject
+            room: $room
+          }
+        ) {
+          ...ClassFields
+        }
+      }
+      ${CLASS_FIELDS}
+    `,
     ({
       name,
       id, // random invite string of length 4
@@ -59,62 +60,62 @@ ${CLASS_FIELDS}
 
 export const classesByUserID = ({ id }) =>
   queryOp(
-    `
-query ClassesByUserID($id: ID!) {
-  result: findUserByID(id: $id) {
-    _id
-    teaches {
-      data {
-        ...ClassFields
+    gql`
+      query ClassesByUserID($id: ID!) {
+        result: findUserByID(id: $id) {
+          _id
+          teaches {
+            data {
+              ...ClassFields
+            }
+          }
+          attends {
+            data {
+              ...ClassFields
+            }
+          }
+        }
       }
-    }
-    attends {
-      data {
-        ...ClassFields
-      }
-    }
-  }
-}
-${CLASS_FIELDS}
-`,
+      ${CLASS_FIELDS}
+    `,
     { id }
   );
 
 export const usersByClassID = ({ classID }) =>
   queryOp(
-    `
-query FindUsersByClassID($id: ID!) {
-  result: findClassByID(id: $id) {
-    ...ClassFields
-    teachers {
-      data {
-        name
-        _id
+    gql`
+      query FindUsersByClassID($id: ID!) {
+        result: findClassByID(id: $id) {
+          ...ClassFields
+          teachers {
+            data {
+              name
+              _id
+            }
+          }
+          students {
+            data {
+              name
+              _id
+            }
+          }
+        }
       }
-    }
-    students {
-      data {
-        name
-        _id
-      }
-    }
-  }
-}
-${CLASS_FIELDS}
-`,
+      ${CLASS_FIELDS}
+    `,
     { id: classID }
   );
 
 export const findClass = ({ classID }) =>
   queryOp(
-    `
-query FindClassByID($id: ID!) {
-  result: findClassByID(id: $id) {
-    ...ClassFields
-  }
-}
-${CLASS_FIELDS}
-`,
+    gql`
+      query FindClassByID($id: ID!) {
+        result: findClassByID(id: $id) {
+          ...ClassFields
+        }
+      }
+      ${CLASS_FIELDS}
+    `,
     { id: classID }
   );
 
@@ -122,105 +123,136 @@ ${CLASS_FIELDS}
  * warn: Seems difficult to secure on the client side with privileges. In a prod env, likely easier to handle logic via serverless func.
  */
 export const useJoinClass = () =>
-  useMutation(`mutation JoinClass($userID: ID!, $classID: ID!) {
-    result: partialUpdateClass(
-      id: $classID
-      data: { students: { connect: [$userID] } }
-    ) {
-      ...ClassFields
+  useMutation(gql`
+    mutation JoinClass($userID: ID!, $classID: ID!) {
+      result: partialUpdateClass(
+        id: $classID
+        data: { students: { connect: [$userID] } }
+      ) {
+        ...ClassFields
+      }
     }
-  }
-  ${CLASS_FIELDS}`);
+    ${CLASS_FIELDS}
+  `);
 
 export const findStreams = ({ classID }) =>
   queryOp(
-    `
-query findStreams($classID: ID!) {
-  result: findClassByID(id: $classID) {
-    streams {
-      data {
-        _id
-        message
-        author {
-          _id
-          name
-        }
-        _ts
-        comments {
-          data {
-            _id
-            author {
-              name
+    gql`
+      query findStreams($classID: ID!) {
+        result: findClassByID(id: $classID) {
+          streams {
+            data {
+              _id
+              message
+              author {
+                _id
+                name
+              }
+              _ts
+              comments {
+                data {
+                  _id
+                  author {
+                    name
+                  }
+                  message
+                  _ts
+                }
+              }
             }
-            message
-            _ts
           }
         }
       }
-    }
-  }
-}
-`,
+    `,
     { classID }
   );
 
 export const useCreateStream = () =>
-  useMutation(`
-mutation CreateStream($userID: ID!, $classID: ID!, $message: String!) {
-  result: createStream(
-    data: {
-      author: { connect: $userID }
-      class: { connect: $classID }
-      message: $message
+  useMutation(gql`
+    mutation CreateStream($userID: ID!, $classID: ID!, $message: String!) {
+      result: createStream(
+        data: {
+          author: { connect: $userID }
+          class: { connect: $classID }
+          message: $message
+        }
+      ) {
+        _id
+        message
+        _ts
+        author {
+          name
+          _id
+        }
+      }
     }
-  ) {
-    _id
-    message
-    _ts
-    author {
-      name
-      _id
-    }
-  }
-}
-`);
+  `);
 
 export const useCreateComment = () =>
-  useMutation(`
-mutation CreateComment($userID: ID!, $streamID: ID!, $message: String!) {
-  result: createComment(
-    data: {
-      author: { connect: $userID }
-      stream: { connect: $streamID }
-      message: $message
+  useMutation(gql`
+    mutation CreateComment($userID: ID!, $streamID: ID!, $message: String!) {
+      result: createComment(
+        data: {
+          author: { connect: $userID }
+          stream: { connect: $streamID }
+          message: $message
+        }
+      ) {
+        _id
+        message
+        _ts
+        stream {
+          _id
+          message
+        }
+        author {
+          name
+          _id
+        }
+      }
     }
-  ) {
-    _id
-    message
-    _ts
-    stream {
-      _id
-      message
-    }
-    author {
-      name
-      _id
-    }
-  }
-}
-`);
+  `);
 
 export const useAddTeacher = () =>
-  useMutation(`
-mutation AddTeacher($classID: ID!, $userID: ID!) {
-  partialUpdateClass(
-    id: $classID
-    data: {
-      teachers: { connect: [$userID] }
-      students: { disconnect: [$userID] }
+  useMutation(gql`
+    mutation AddTeacher($classID: ID!, $userID: ID!) {
+      partialUpdateClass(
+        id: $classID
+        data: {
+          teachers: { connect: [$userID] }
+          students: { disconnect: [$userID] }
+        }
+      ) {
+        _id
+      }
     }
-  ) {
-    _id
-  }
-}
-`);
+  `);
+
+export const useCreateAssignment = () =>
+  useMutation(gql`
+    mutation CreateAssignment(
+      $title: String!
+      $text: String!
+      $points: Int!
+      $due: Date!
+      $type: AssignmentType!
+      $created: Time!
+      $assignees: [ID]!
+      $creator: ID!
+    ) {
+      result: createAssignment(
+        data: {
+          title: $title
+          text: $text
+          points: $points
+          due: $due
+          type: $type
+          created: $created
+          assignees: { connect: $assignees }
+          creator: { connect: $creator }
+        }
+      ) {
+        _id
+      }
+    }
+  `);
