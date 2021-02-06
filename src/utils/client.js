f5f82e9ea797d3118f4c3e31123f9ee2683d8136
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-02-06 05:56:47 -0800

    remove fluff

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,6 +1,5 @@
 import {
-  setClient,
-  createClient,
+  initClient as urqlInit,
   operationStore,
   query,
   mutation,
@@ -8,46 +7,40 @@ import {
 } from "@urql/svelte";
 import { devtoolsExchange } from "@urql/devtools"; // TODO: for dev only. Remove on prod
 
-/**
- * Function to create a new urql client
- */
-export function initClient(secret) {
-  return setClient(
-    createClient({
-      url: "https://graphql.fauna.com/graphql",
-      exchanges: [devtoolsExchange, ...defaultExchanges], // TODO: for dev only. Remove on prod
-      fetchOptions() {
-        return {
-          headers: {
-            authorization: secret ? `Bearer ${secret}` : "",
-            "X-Schema-Preview": "partial-update-mutation",
-          },
-        };
-      },
-    })
-  );
-}
+/** urql preconfigured fauna client */
+export const initClient = (secret) =>
+  urqlInit({
+    url: "https://graphql.fauna.com/graphql",
+    exchanges: [devtoolsExchange, ...defaultExchanges], // TODO: for dev only. Remove on prod
+    fetchOptions() {
+      return {
+        headers: {
+          authorization: secret ? `Bearer ${secret}` : "",
+          "X-Schema-Preview": "partial-update-mutation",
+        },
+      };
+    },
+  });
 
 /** Creates a query for a component to consume.
- * @param {string} gqlQuery
  * @param {object} variables
  */
 export const queryOp = (gqlQuery, variables) =>
   query(operationStore(gqlQuery, variables));
 
 /** Creates a mutation hook for a component to consume.
- * @param {string} gqlMutation - A template literal with your GraphQL mutation.
  * @param {function} cbVarsObj A callback that should destructure & restructure the object with variables passed in by
  * the component. It can set defaults/fallbacks and shape its returned variables object. If none specified, will pass var obj supplied unmodified.
  * Example:
  * ```js
  * ({ name = "no name entered", id }) => ({ name, id })
  * ```
- * @returns {Array} A tuple with the mutation function and its store. Final tuple should only be retrieved by calling
- * in a component.
+ * @returns {Array} Tuple w/ mutation & store. Call tuple within component.
  * Usage:
  * ```js
  * const [noteCreate, noteStore] = useCreateNote();
+ * $noteStore
+ * noteCreate(variablesObj)
  * ```
  */
 export function useMutation(gqlMutation, cbVarsObj = (variables) => variables) {
